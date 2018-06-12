8bb67da826eb3bd59022b71282f93e7c1187ffda
Mo-Wei <ibarca9ff@gmail.com>
2018-06-12 16:57:22 +0800

    Updata

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -3,9 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class BaseController extends Controller {
     public function index(){
-        $data = M('document');
-        $datas = $data -> select();
-        $this->assign('datas',$datas);
         $this->display();
     }
 }
